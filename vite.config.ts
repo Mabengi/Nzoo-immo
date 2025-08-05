02c543cd59ebe11ba43e4703786f15d09b3d8de1
alydabonsey <tricksonmabengi123@gmail.com>
2025-08-05 19:02:27 +0100

    Fix vite config and add _redirects for React Router SPA

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 4173,
     allowedHosts: ['nzooimmo.com', 'www.nzooimmo.com'],
-  }
+  },
+  base: '/',  // à ajouter si ce n’est pas déjà fait
 });
+
 
